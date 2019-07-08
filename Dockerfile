dcaf6e500e376c4ea04c303e93df6ee303722f47
napadlekp <piotr.napadlek@guestline.com>
2019-07-08 16:39:27 +0200

    added dockerfile; basic azure pipeline

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM maven:3.6.1-jdk-12 as build
+WORKDIR /workspace/app
+
+COPY settings.xml .
+
+COPY pom.xml .
+RUN mvn dependency:go-offline -s settings.xml
+
+COPY src src
+RUN mvn clean package -s settings.xml
+
+RUN mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+
+FROM openjdk:12.0.1-jdk
+
+VOLUME /tmp
+
+ARG DEPENDENCY=/workspace/app/target/dependency
+COPY --from=build ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY --from=build ${DEPENDENCY}/META-INF /app/META-INF
+COPY --from=build ${DEPENDENCY}/BOOT-INF/classes /app
+
+ENTRYPOINT ["java", "-cp", "app:app/lib/*", "io.napadlek.eventhubbrowser.EventhubBrowserApplicationKt"]
--- src/main/kotlin/io/napadlek/eventhubbrowser/message/MessageQueryParams.kt
+++ src/main/kotlin/io/napadlek/eventhubbrowser/message/MessageQueryParams.kt
@@ -18,11 +18,16 @@ data class MessageQueryParams(private val queryProperties: MultiValueMap<String,
                 ?: LocalDateTime.MIN.atZone(ZoneOffset.UTC)
         val toTimestamp = queryProperties["toTimestamp"]?.firstOrNull()?.let { ZonedDateTime.parse(it) }
                 ?: LocalDateTime.MAX.atZone(ZoneOffset.UTC)
+
         timestampRange = fromTimestamp..toTimestamp
+
         sequenceNumberRange = (queryProperties["fromSequenceNumber"]?.firstOrNull()?.toLong() ?: -1L)..
                 (queryProperties["toSequenceNumber"]?.firstOrNull()?.toLong() ?: Long.MAX_VALUE)
+
         offsetRange = (queryProperties["fromOffset"]?.firstOrNull()?.toLong() ?: 0)..(queryProperties["toOffset"]?.firstOrNull()?.toLong() ?: Long.MAX_VALUE)
+
         partitionKey = queryProperties["partitionKey"]?.firstOrNull()
+
         properties = queryProperties["property"]?.map {
             val stringSplit = it.split("=", limit = 2)
             stringSplit[0] to stringSplit.getOrNull(1)
